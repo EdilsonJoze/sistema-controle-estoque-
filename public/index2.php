75ab053016e5582e98a6e9122b3c55f7b67a8e7c
EdilsonJoze <133261990+EdilsonJoze@users.noreply.github.com>
2026-08-14 14:30:52 -0300

    Update index2.php

--- public/index2.php
+++ public/index2.php
@@ -1,3 +1,6 @@
 $router->add('GET', 'produtos', 'ProdutoController', 'index');
 $router->add('GET', 'produtos/cadastrar', 'ProdutoController', 'criar');
 $router->add('POST', 'produtos/salvar', 'ProdutoController', 'salvar');
+$router->add('GET', 'produtos/editar', 'ProdutoController', 'editar');
+$router->add('POST', 'produtos/atualizar', 'ProdutoController', 'atualizar');
+$router->add('POST', 'produtos/deletar', 'ProdutoController', 'deletar');
